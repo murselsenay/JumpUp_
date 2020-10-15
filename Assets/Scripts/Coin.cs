b1df9f9e9149da4a8748ed757ad4e5f03b059897
Mürsel Şenay <murselsenay@gmail.com>
2020-10-15 16:26:01 +0300

    15102020

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public GameObject coinCollectingEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            Instantiate(coinCollectingEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
