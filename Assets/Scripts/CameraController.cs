8319a1a2f035b63211451c8ac9625f779c604055
murselsenay <murselsenay@gmail.com>
2020-10-18 22:28:32 +0300

    18102020

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,12 @@ public class CameraController : MonoBehaviour
     bool canGoUp = false;
     public float cameraSpeed = 0.2f;
     public bool warn = false;
+
+    public int floorCount;
     // Start is called before the first frame update
     void Start()
     {
-
+        floorCount = floorCount - 1;
     }
 
     // Update is called once per frame
@@ -35,7 +37,10 @@ public class CameraController : MonoBehaviour
 
         Vector2 move = new Vector2(0f, cameraSpeed);
         transform.Translate(move, Space.Self);
-
+        if (transform.position.y > floorCount * 9f)
+        {
+            canGoUp = false;
+        }
     }
 
     void WarningMessage()
